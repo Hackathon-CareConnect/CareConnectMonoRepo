961f9c11c4b8bb90653285596190cc7d23c22ba7
Zach Lyons <zachlyons1212@gmail.com>
2024-10-23 15:10:43 -0400

    mongo atlas connection

---
+++ ReactNativeApp/screens/SignUpScreen.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Please </Text>
+      
+      <Button
+        title="Sign up"
+        onPress={() => navigation.navigate('SignUp')}
+        />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
+export default SignUpScreen;

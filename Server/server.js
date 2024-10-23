9ea0465aaa8c9c65b8fec645984bcb1431b36440
Zach Lyons <zachlyons1212@gmail.com>
2024-10-23 15:10:43 -0400

    mongo atlas connection

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,5 @@
-const express = require('express');
 const mongoose = require('mongoose');
+const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth');

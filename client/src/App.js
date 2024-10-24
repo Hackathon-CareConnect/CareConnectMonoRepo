c1f105a3f84d11a7100f9e477430e83d2938319d
ReactRepo <barrerojuan810@gmail.com>
2024-10-24 12:10:10 -0400

    Route for createAccount in the Login view below the signIn button

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Login from './components/Login';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CreateAccount from './components/CreateAccount';
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <div className='div1'>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path='register' element={<CreateAccount />}></Route>
+      </Routes>
+      </BrowserRouter>
+    </div>    
   );
 }
 
